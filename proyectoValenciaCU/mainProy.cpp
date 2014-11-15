f2b035b4050d668de2d9fcd48809254e83dee789
magooz1963@hotmail.com <magooz1963@hotmail.com>
2014-11-15 16:34:28 -0600

    segundo commit

---
+++ proyectoValenciaCU/mainProy.cpp
@@ -0,0 +1,314 @@
+//#include <gl/gl.h>     // The GL Header File
+//#include <windows.h>	//Solo para Windows
+//#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
+//#include <stdlib.h>		// Descomentar para Linux
+//#include <stdio.h>
+//#include <string.h>
+//#include <malloc.h>		// Solo para Windows
+
+#include "texture.h"
+#include "figuras.h"
+#include "Camera.h"
+#include "cmodel/CModel.h"
+
+CModel rectoria;
+CModel facultadD;
+
+
+//NEW//////////////////NEW//////////////////NEW//////////////////NEW////////////////
+CCamera objCamera;
+
+GLfloat g_lookupdown = 0.0f;    // Look Position In The Z-Axis (NEW) 
+//NEW//////////////////NEW//////////////////NEW//////////////////NEW////////////////
+
+int font = (int)GLUT_BITMAP_HELVETICA_18;
+
+//Otras opciones son:
+/*GLUT_BITMAP_8_BY_13
+GLUT_BITMAP_9_BY_15
+GLUT_BITMAP_TIMES_ROMAN_10
+GLUT_BITMAP_TIMES_ROMAN_24
+GLUT_BITMAP_HELVETICA_10
+GLUT_BITMAP_HELVETICA_12
+GLUT_BITMAP_HELVETICA_18*/
+
+
+
+GLUquadricObj *quadratic;				// Storage For Our Quadratic Objects ( NEW )
+
+
+//GLfloat Diffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };				// Diffuse Light Values
+GLfloat Diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };				// Diffuse Light Values
+GLfloat Specular[] = { 1.0, 1.0, 1.0, 1.0 };				// Specular Light Values
+GLfloat Position[] = { 0.0f, 7.0f, -5.0f, 0.0f };			// Light Position
+GLfloat Position2[] = { 0.0f, 0.0f, -5.0f, 1.0f };			// Light Position
+
+GLfloat m_dif1[] = { 0.0f, 0.2f, 1.0f, 1.0f };				// Diffuse Light Values
+GLfloat m_spec1[] = { 0.0, 0.0, 0.0, 1.0 };				// Specular Light Values
+GLfloat m_amb1[] = { 0.0, 0.0, 0.0, 1.0 };				// Ambiental Light Values
+GLfloat m_s1[] = { 18 };
+
+GLfloat m_dif2[] = { 0.8f, 0.2f, 0.0f, 1.0f };				// Diffuse Light Values
+GLfloat m_spec2[] = { 0.0, 0.0, 0.0, 1.0 };				// Specular Light Values
+GLfloat m_amb2[] = { 0.0, 0.0, 0.0, 1.0 };				// Ambiental Light Values
+GLfloat m_s2[] = { 22 };
+
+
+
+float colorR = 0.0;
+float colorG = 0.0;
+float colorB = 0.0;
+
+
+void InitGL()     // Inicializamos parametros
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo	
+
+	glEnable(GL_TEXTURE_2D);
+
+	glShadeModel(GL_SMOOTH);
+	//Para construir la figura comentar esto
+	glLightfv(GL_LIGHT1, GL_POSITION, Position);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, Diffuse);
+	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, Position2);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHT1);
+
+	glEnable(GL_COLOR_MATERIAL);					//Habilitar GlColor3f
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	//glPolygonMode(GL_BACK, GL_LINE);
+
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+
+	/* setup blending */
+	//glBlendFunc(GL_SRC_ALPHA,GL_ONE);			// Set The Blending Function For Translucency
+	//glColor4f(1.0f, 1.0f, 1.0f, 0.5); 
+
+	rectoria._3dsLoad("rectoria/rectoria.3ds");
+	rectoria.LoadTextureImages();
+	rectoria.GLIniTextures();
+	rectoria.ReleaseTextureImages();
+
+	facultadD._3dsLoad("derecho3DS/derecho.3ds");
+	facultadD.LoadTextureImages();
+	facultadD.GLIniTextures();
+	facultadD.ReleaseTextureImages();
+
+	quadratic = gluNewQuadric();			// Create A Pointer To The Quadric Object ( NEW )
+	gluQuadricNormals(quadratic, GLU_SMOOTH);	// Create Smooth Normals ( NEW )
+	gluQuadricTexture(quadratic, GL_TRUE);		// Create Texture Coords ( NEW )
+
+
+	objCamera.Position_Camera(0, 2.5f, 3, 0, 2.5f, 0, 0, 1, 0);
+
+}
+
+void pintaTexto(float x, float y, float z, void *font, char *string)
+{
+
+	char *c;     //Almacena los caracteres a escribir
+	glRasterPos3f(x, y, z);	//Posicion apartir del centro de la ventana
+	//glWindowPos2i(150,100);
+	for (c = string; *c != '\0'; c++) //Condicion de fin de cadena
+	{
+		glutBitmapCharacter(font, *c); //imprime
+	}
+}
+
+
+void display(void)   // Creamos la funcion donde se dibuja
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+
+
+	glPushMatrix();
+		glRotatef(g_lookupdown, 1.0f, 0, 0);
+
+		gluLookAt(objCamera.mPos.x, objCamera.mPos.y, objCamera.mPos.z,
+			objCamera.mView.x, objCamera.mView.y, objCamera.mView.z,
+			objCamera.mUp.x, objCamera.mUp.y, objCamera.mUp.z);
+
+
+		glPushMatrix();
+
+			glPushMatrix(); //Rectoria
+				glDisable(GL_LIGHTING);
+				glTranslatef(-230, 0.0,50.0);
+					glScalef(0.009, 0.009, 0.009);
+					rectoria.GLrender(NULL, _SHADED, 1);
+				glEnable(GL_LIGHTING);
+			glPopMatrix();
+
+			glPushMatrix(); //facultad de derecho
+				glDisable(GL_LIGHTING);
+					glTranslatef(-32.0, 0.0, -9.0);
+					glScalef(0.7, 0.7, 0.7);
+					facultadD.GLrender(NULL, _SHADED, 1);
+				glEnable(GL_LIGHTING);
+			glPopMatrix();
+			glColor3f(1.0, 1.0, 1.0);		//Para que los demas elementos regresen a su color de su textura y no permanescan rojos
+		glPopMatrix();
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0, 0.0, 0.0);
+	pintaTexto(-0.25, 0.23, -0.25, (void *)font, "Practica 9");
+	pintaTexto(-0.25, 0.21, -0.25, (void *)font, "Textura en Movimiento");
+	glColor3f(1.0, 1.0, 1.0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_TEXTURE_2D);
+
+	glutSwapBuffers();
+
+}
+
+void animacion()
+{
+
+}
+
+void reshape(int width, int height)   // Creamos funcion Reshape
+{
+	if (height == 0)										// Prevenir division entre cero
+	{
+		height = 1;
+	}
+
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+
+	glFrustum(-0.1, 0.1, -0.1, 0.1, 0.1, 300.0);
+
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)  // Create Keyboard Function
+{
+	switch (key) {
+	case 'w':   //Movimientos de camara
+	case 'W':
+		objCamera.Move_Camera(CAMERASPEED + 0.2);
+		break;
+
+	case 's':
+	case 'S':
+		objCamera.Move_Camera(-(CAMERASPEED + 0.2));
+		break;
+
+	case 'a':
+	case 'A':
+		objCamera.Strafe_Camera(-(CAMERASPEED + 0.4));
+		break;
+
+	case 'd':
+	case 'D':
+		objCamera.Strafe_Camera(CAMERASPEED + 0.4);
+		break;
+
+	case 'r':		//
+	case 'R':
+
+		break;
+
+	case 'f':		//
+	case 'F':
+
+		break;
+
+	case 27:        // Cuando Esc es presionado...
+		exit(0);   // Salimos del programa
+		break;
+	case 'p':
+		//pos_camY -= 0.5f;
+		//pos_cam.y += 0.5f;
+		//eye_cam.y += 0.5f;
+		objCamera.UpDown_Camera(CAMERASPEED);
+		break;
+
+	case 'l':
+		//pos_camY += 0.5f;
+		//pos_cam.y -= 0.5f;
+		//eye_cam.y -= 0.5f;
+		objCamera.UpDown_Camera(-CAMERASPEED);
+		break;
+	default:        // Cualquier otra
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void arrow_keys(int a_keys, int x, int y)  // Funcion para manejo de teclas especiales (arrow keys)
+{
+	switch (a_keys) {
+	case GLUT_KEY_PAGE_UP:
+		//pos_camY -= 0.5f;
+		//pos_cam.y += 0.5f;
+		//eye_cam.y += 0.5f;
+		objCamera.UpDown_Camera(CAMERASPEED);
+		break;
+
+	case GLUT_KEY_PAGE_DOWN:
+		//pos_camY += 0.5f;
+		//pos_cam.y -= 0.5f;
+		//eye_cam.y -= 0.5f;
+		objCamera.UpDown_Camera(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		g_lookupdown -= 1.0f;
+		break;
+
+	case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		g_lookupdown += 1.0f;
+		break;
+
+	case GLUT_KEY_LEFT:
+		objCamera.Rotate_View(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_RIGHT:
+		objCamera.Rotate_View(CAMERASPEED);
+		break;
+
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+
+int main(int argc, char** argv)   // Main Function
+{
+	glutInit(&argc, argv); // Inicializamos OpenGL
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+	glutInitWindowSize(500, 500);	// Tamaño de la Ventana
+	glutInitWindowPosition(0, 0);	//Posicion de la Ventana
+	glutCreateWindow("Practica 9"); // Nombre de la Ventana
+	//glutFullScreen     ( );         // Full Screen
+	InitGL();						// Parametros iniciales de la aplicacion
+	glutDisplayFunc(display);  //Indicamos a Glut función de dibujo
+	glutReshapeFunc(reshape);	//Indicamos a Glut función en caso de cambio de tamano
+	glutKeyboardFunc(keyboard);	//Indicamos a Glut función de manejo de teclado
+	glutSpecialFunc(arrow_keys);	//Otras
+	glutIdleFunc(animacion);
+	glutMainLoop();          // 
+
+	return 0;
+}
